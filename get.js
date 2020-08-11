8e326c8f8e0f711643a8bac9ee423e68d32e2e65
Brett Fieber <bfieber@loanpal.com>
2020-08-11 15:50:44 -0700

    Adding a timeout

--- get.js
+++ get.js
@@ -20,7 +20,7 @@ export const main = handler(async (event, context) => {
 
 
   // Set a timeout
-  await new Promise(resolve => settimeout(resolve, 10000));
+  await new Promise(resolve => setTimeout(resolve, 10000));
 
   // Return the retrieved item
   return result.Item;
